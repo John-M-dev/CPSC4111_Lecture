44b12ea9f618de7c4a3ccb2bf75679009810063b
John <52011445+John-M-dev@users.noreply.github.com>
2023-09-19 16:20:11 -0400

    Lecture Update 9-19-2023
    Fixed movement to be more random
    Renamed function
    Added Player network movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,18 +42,18 @@ namespace FirstMultiplayer
 
         static void SubmitNewPosition()
         {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
+            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move?" : "Request Position Change"))
             {
                 if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient )
                 {
                     foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().Move();
+                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().RandomSpawn();
                 }
                 else
                 {
                     var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
                     var player = playerObject.GetComponent<Player>();
-                    player.Move();
+                    player.RandomSpawn();
                 }
             }
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,24 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace FirstMultiplayer
 {
     public class Player : NetworkBehaviour
     {
+        public float speed = 10.0f;
+        
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
         public override void OnNetworkSpawn()
         {
             if (IsOwner)
             {
-                Move();
+                RandomSpawn();
             }
         }
 
-        public void Move()
+        public void RandomSpawn()
         {
             if (NetworkManager.Singleton.IsServer)
             {
@@ -37,12 +40,28 @@ namespace FirstMultiplayer
 
         static Vector3 GetRandomPositionOnPlane()
         {
-            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+            return new Vector3(Random.Range(-3f, 3f),Random.Range(-3f, 3f), 0f);
         }
 
         void Update()
         {
+            if (IsOwner)
+            {
+                float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+                float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+                
+                MovePlayerServerRPC(horizontal, vertical);
+            }
+
+            
             transform.position = Position.Value;
+        }
+
+        [ServerRpc]
+        void MovePlayerServerRPC(float horizontal, float vertical)
+        {
+            transform.Translate(horizontal, vertical, 0);
+            Position.Value = transform.position;
         }
     }
 }
