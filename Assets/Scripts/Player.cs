53b75575c07af0f084e689fcceafeebcb7b21ee4
John <52011445+John-M-dev@users.noreply.github.com>
2023-09-26 16:16:56 -0400

    Lecture 9-26-2023 Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,41 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace FirstMultiplayer
 {
     public class Player : NetworkBehaviour
     {
-        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
         public float speed = 10.0f;
+        
+        //public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
         public override void OnNetworkSpawn()
         {
             if (IsOwner)
             {
                 RandomSpawn();
+                RequestPlayerColorServerRpc();
+            }
+            else
+            {
+                UpdatePlayerColorsServerRpc();
+            }
+        }
+        
+        [ServerRpc]
+        public void UpdatePlayerColorsServerRpc()
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+                {
+                    Player player = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>();
+                    //NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().RandomSpawn();
+                    player.RandomSpawn();
+                    Color color = player.gameObject.GetComponent<Renderer>().material.color;
+                    player.SetPlayerColorClientRpc(color);
+                }
             }
         }
 
@@ -21,7 +45,12 @@ namespace FirstMultiplayer
             {
                 var randomPosition = GetRandomPositionOnPlane();
                 transform.position = randomPosition;
-                Position.Value = randomPosition;
+
+                
+                
+                // Renderer r = GetComponent<Renderer>();
+                // r.material.color = Random.ColorHSV();
+                //Position.Value = randomPosition;
             }
             else
             {
@@ -32,34 +61,48 @@ namespace FirstMultiplayer
         [ServerRpc]
         void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
         {
-            Position.Value = GetRandomPositionOnPlane();
+            // Position.Value = GetRandomPositionOnPlane();
+            transform.position = GetRandomPositionOnPlane();
         }
 
         static Vector3 GetRandomPositionOnPlane()
         {
-            return new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 1f );
+            return new Vector3(Random.Range(-3f, 3f),Random.Range(-3f, 3f), 0f);
+        }
+
+        [ServerRpc]
+        void RequestPlayerColorServerRpc(ServerRpcParams rpcParams = default)
+        {
+            Color color = Random.ColorHSV();
+            gameObject.GetComponent<Renderer>().material.color = color;
+            SetPlayerColorClientRpc(color);
         }
 
         void Update()
         {
-
-            if(IsOwner)
+            if (IsOwner)
             {
                 float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
                 float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-
-                //transform.Translate(horizontal, vertical, 0);
-                MovePlayerServerRpc(horizontal, vertical);
+                
+                MovePlayerServerRPC(horizontal, vertical);
             }
 
-            //transform.position = Position.Value;
+            
+            // transform.position = Position.Value;
         }
 
         [ServerRpc]
-        void MovePlayerServerRpc(float horizontal, float vertical)
+        void MovePlayerServerRPC(float horizontal, float vertical)
         {
             transform.Translate(horizontal, vertical, 0);
-            //Position.Value = transform.position;
+            // Position.Value = transform.position;
+        }
+
+        [ClientRpc]
+        public void SetPlayerColorClientRpc(Color newColor)
+        {
+            GetComponent<Renderer>().material.color = newColor;
         }
     }
 }
